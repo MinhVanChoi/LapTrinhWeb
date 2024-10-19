5725a522f590cbb6385988b7b341851b80a95598
Minh Văn <149926277+MinhVanChoi@users.noreply.github.com>
2024-10-20 00:59:05 +0700

    commit

--- demo/src/main/java/vn/iotstar/controllers/admin/AdminHomeController.java
+++ demo/src/main/java/vn/iotstar/controllers/admin/AdminHomeController.java
@@ -4,13 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
 @Controller
-@RequestMapping(value="/admin")
+@RequestMapping(value = "/admin")
 public class AdminHomeController {
-	@GetMapping("/")
-	public String index()
-	{
-		return "/admin/home";
-		
-	}
+    @GetMapping("/home")
+    public String index() {
+        return "admin/home";
+    }
 }
